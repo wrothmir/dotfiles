53488f2e09c2fd07b13a35e74327232e14bb430c
Vineet Agarwal <vineetagarwal2402@gmail.com>
2024-09-01 15:15:29 -0700

    Add NixOS

---
+++ nixos/flake.nix
@@ -0,0 +1,28 @@
+{
+  description = "Nixos config flake";
+
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
+
+    home-manager = {
+      url = "github:nix-community/home-manager";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+
+    hyprland.url = "github:hyprwm/Hyprland";
+    hyprland-plugins = {
+      url = "github:hyprwm/hyprland-plugins";
+      inputs.hyprland.follows = "hyprland";
+    };
+  };
+
+  outputs = { self, nixpkgs, home-manager, hyprland, ... }@inputs: {
+    nixosConfigurations.default = nixpkgs.lib.nixosSystem {
+      specialArgs = {inherit inputs;};
+      modules = [
+        ./hosts/default/configuration.nix
+        inputs.home-manager.nixosModules.default
+      ];
+    };
+  };
+}
